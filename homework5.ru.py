4afedebfc29948380590ec4a00568ae1ebba197d
VladaPolonskaya <vlada-prim@mail.ru>
2024-07-23 00:15:37 +1000

    Add files via upload

---
+++ homework5.ru.py
@@ -0,0 +1,7 @@
+immutable_var = (1, 2, 3, 4, True, 'string')
+print('immutable_var:',(immutable_var))
+#immutable_var[1] = 10
+#print(immutable_var)
+mutable_list = [1, 2, 'a', 'c', 'string']
+mutable_list[2] = 'b'
+print('mutable_list:',(mutable_list))
